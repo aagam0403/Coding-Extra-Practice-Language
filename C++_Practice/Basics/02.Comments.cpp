e4e96ac9cf8ed50e273075ef77df054b86091e3e
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-06-10 12:07:38 +0530

    Add files via upload

---
+++ C++_Practice/Basics/02.Comments.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+//Comments in C++
+    // Single-line comment starts with // like this line
+    
+    /*Multi-line comment starts with /* and ends with */
+}
