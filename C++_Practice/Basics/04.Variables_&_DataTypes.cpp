9ef5433c304f2c75aee0840066de092b63cce279
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-06-11 13:49:19 +0530

    Add files via upload

---
+++ C++_Practice/Basics/04.Variables_&_DataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int age = 20;         // Integer
+    float weight = 65.5;  // Floating point
+    char grade = 'A';     // Character
+    bool isPass = true;   // Boolean
+    string name = "Aagam";// String (from C++ Standard Library)
+
+    cout << "Age: " << age << ", Weight: " << weight << endl;
+    cout << "Grade: " << grade << ", Passed: " << isPass << endl;
+    cout << "Name: " << name << endl;
+}
