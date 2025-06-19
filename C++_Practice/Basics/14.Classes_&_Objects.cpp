5d4abc24169d4c07f071910aa1d53fd5a1994498
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-06-19 19:13:51 +0530

    Add files via upload

---
+++ C++_Practice/Basics/14.Classes_&_Objects.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   //Classes and Objects
+    class Car {
+    public:
+        string brand;
+        int year;
+
+        void start() {
+            cout << brand << " is starting..." << endl;
+        }
+    } myCar;
+
+    myCar.brand = "Toyota";
+    myCar.year = 2022;
+    myCar.start();
+    return 0;
+}
