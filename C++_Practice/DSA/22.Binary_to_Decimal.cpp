2dc97663a2aa433a695e3a86b024d66aa505e93d
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-07-14 14:34:27 +0530

    Add files via upload

---
+++ C++_Practice/DSA/22.Binary_to_Decimal.cpp
@@ -0,0 +1,22 @@
+//Binary Number System
+#include <iostream>
+using namespace std;
+
+int binToDecimal (int binNum)
+{
+    int ans = 0, pow = 1;
+    while(binNum>0)
+    {
+        int rem = binNum %10;
+        ans += rem * pow;
+        
+        binNum /= 10;
+        pow *= 2;
+    }
+    return ans; //Decimal Form
+}
+int main() 
+{
+    cout<<binToDecimal(110010) << endl;
+    return 0;
+}
